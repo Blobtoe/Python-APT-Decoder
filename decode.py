1e260f381eb2fe48b2bef8e6460acf405104fbd6
Blobtoe <felixxperron@gmail.com>
2020-05-10 22:10:44 -0700

    commti name lol

--- decode.py
+++ decode.py
@@ -58,15 +58,21 @@ def decode(samples):
 if __name__ == "__main__":
     targetRate = 20800
 
-    rate, data = wavfile.read(sys.argv[0])
+    rate, data = wavfile.read(sys.argv[1])
     print("Loaded audio file")
 
     data = signal.resample(data, len(data)//rate*targetRate+(targetRate//2))
-    print("Audio resampled to {}".format(targetRate))
+    print("Resampled to {}".format(targetRate))
 
 
     data = decode(data)
     im = Image.fromarray(data)
-    print("Showing image...")
-    im.show()
-    im.convert("RGB").save(sys.argv[1], "PNG")
+    try:
+        if sys.argv[3] == "show":
+            print("Showing image...")
+            im.show()
+        else:
+            print("Unknown argument: {}".format(sys.argv[3]))
+    except:
+        pass
+    im.convert("RGB").save(sys.argv[2], "PNG")
